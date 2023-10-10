63594dcfca0d856b6a68ae58f4790eda46314656
Павел Данилевич <pasha-danilevich@outlook.com>
2023-10-11 00:09:12 +0300

    Сделал 2 задания
    удалил ненужные, неинтересные папки

--- Data_types_and_structures/tuple.py
+++
@@ -1,5 +0,0 @@
-tuple_empty = ()
-
-four_el_tuple = ((1,2,4),[2,3,5],{'йцу':'qwe','key':True},True)
-print(type(four_el_tuple))
-print(iter('f'))
---
+++ quest/2.py
@@ -0,0 +1,38 @@
+# Question: Write a program which can compute the factorial 
+# of a given numbers. The results should be printed in a 
+# comma-separated sequence on a single line. Suppose the following 
+# input is supplied to the program: 8 Then, the output should be: 40320
+
+# My solution
+
+number = 8
+
+
+def factorial(number):
+    result = 1
+    for i in range(1, number):
+        result += i * result
+    return result
+        
+print(factorial(number))
+
+# Current solution
+
+def fact(x):
+    if x == 0:
+        return 1
+    else:
+        return fact(x - 1) * x
+    
+print(fact(8))
+
+
+mylist = (x*x for x in range(3))
+for i in mylist :
+    print(i)
+    
+    
+for i in mylist :
+    print(i)
+
+        
